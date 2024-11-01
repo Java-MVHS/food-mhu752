27e773a1cdeb1aa482233c7d1944ef694f80391d
mhu752 <mhu752@student.fuhsd.org>
2024-10-31 22:25:30 -0700

    Update Pizza.java

--- Pizza.java
+++ Pizza.java
@@ -2,9 +2,11 @@
 * Megumi Hu p5 10/23/2024 - wednesday
 *Pizza.java
 *
+*This programm is a virtual bake sale. It will print out the amount and cost of the food type.
+*It will also tell you about the ingredient and the method the food is cooked with.
 */
 
-public class Pizza extends Food
+public class Pizza extends Food 
 {
 	protected String ingredientIn; //declaring variables
 	protected String name; 
