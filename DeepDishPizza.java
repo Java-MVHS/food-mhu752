474555e6571ad8331d07d3e10aba08a6ed663f52
mhu752 <mhu752@student.fuhsd.org>
2024-10-31 22:28:15 -0700

    Update DeepDishPizza.java

--- DeepDishPizza.java
+++ DeepDishPizza.java
@@ -1,6 +1,10 @@
 /*
+* Megumi Hu p5 10/23/2024 - wednesday
+*DeepDishPizza.java
+*
+*This programm is a virtual bake sale. It will print out the amount and cost of the food type.
+*It will also tell you about the ingredient and the method the food is cooked with.
 */
-
 public class DeepDishPizza extends Pizza
 {
 	public DeepDishPizza(String ingredientIn) 
