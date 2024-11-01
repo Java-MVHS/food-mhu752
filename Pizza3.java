192d87f5fe67d8603c55de2d9b5bbbb6356699d6
mhu752 <mhu752@student.fuhsd.org>
2024-10-31 22:26:03 -0700

    Update Pizza3.java

--- Pizza3.java
+++ Pizza3.java
@@ -1,3 +1,10 @@
+/*
+* Megumi Hu p5 10/23/2024 - wednesday
+*Pizza3.java
+*
+*This programm is a virtual bake sale. It will print out the amount and cost of the food type.
+*It will also tell you about the ingredient and the method the food is cooked with.
+*/
 public class Pizza3 extends Food3 { //extend food class
     // Constructor for Pizza that accepts only one parameter: the ingredient
     public Pizza3(String ingredient) {
